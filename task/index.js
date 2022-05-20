035530200e103b6aa7235c30cd91b583e73431c3
Ramses Almeida <ramses.almeida@pagar.me>
2022-05-20 11:27:29 -0300

    fix: wrong import

--- task/index.js
+++ task/index.js
@@ -10,7 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const tl = require("azure-pipelines-task-lib/task");
-const runscopeRequirements = "https://gist.githubusercontent.com/ThiagoBarradas/c7c5e83f089108fdd3fad9f165f5aaa0/raw";
+const runscopeRequirements = "https://gist.githubusercontent.com/ramsessacol/37132d91eeb93a32e1067617c34bf98b/raw";
 const runscopeAppPython = "https://gist.githubusercontent.com/ramsessacol/3ee9bbd2009230630cf3ba8c32f83bb9/raw/2cc2da622da3ffab2340c1db29a6364e2987a902";
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
@@ -19,14 +19,17 @@ function run() {
             var triggerUrl = tl.getInput('triggerUrl', true);
             var accessToken = tl.getInput('accessToken', true);
             // install tools
-            yield tl.exec('pip', ["install", "-r", runscopeRequirements]);
+            yield tl.exec('pip3', ["install", "-r", runscopeRequirements]);
             yield tl.exec('wget', ["-O", "app.py", runscopeAppPython]);
+            let result = tl.TaskResult.Succeeded;
             // execute integration tests
-            yield tl.exec('python', ["app.py", triggerUrl, accessToken]);
+            yield tl.exec('python3', ["app.py", triggerUrl, accessToken]);
+            yield tl.setResult(result, null, true);
         }
         catch (err) {
+            tl.error(err.message);
             tl.setResult(tl.TaskResult.Failed, err.message);
         }
     });
 }
-run()
+run();
--- task/index.ts
+++ task/index.ts
@@ -1,6 +1,6 @@
 import * as tl from 'azure-pipelines-task-lib/task';
 
-const runscopeRequirements: string = "https://gist.githubusercontent.com/ThiagoBarradas/c7c5e83f089108fdd3fad9f165f5aaa0/raw";
+const runscopeRequirements: string = "https://gist.githubusercontent.com/ramsessacol/37132d91eeb93a32e1067617c34bf98b/raw";
 const runscopeAppPython: string  = "https://gist.githubusercontent.com/ramsessacol/3ee9bbd2009230630cf3ba8c32f83bb9/raw/2cc2da622da3ffab2340c1db29a6364e2987a902";
 
 async function run() {
@@ -10,13 +10,17 @@ async function run() {
         var accessToken: string = tl.getInput('accessToken', true);
 
         // install tools
-        await tl.exec('pip', ["install", "-r", runscopeRequirements]);
+        await tl.exec('pip3', ["install", "-r", runscopeRequirements]);
         await tl.exec('wget', ["-O", "app.py", runscopeAppPython]);
+
+        let result = tl.TaskResult.Succeeded;
         
         // execute integration tests
-        await tl.exec('python', ["app.py", triggerUrl, accessToken]);
+        await tl.exec('python3', ["app.py", triggerUrl, accessToken]);
+        await tl.setResult(result, null, true);
     }
     catch (err) {
+        tl.error(err.message);
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
 }
